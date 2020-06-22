6b1b1ab8c4463b2eaca6b49773baf02ecfad08ec
Lucas Calebe <lucascalebe07@hotmail.com>
2020-06-22 17:24:45 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -8,15 +8,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -24,14 +26,20 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	// tabela de associação no banco
+	@JoinTable(name = "tb_product_category",
+			// nome coluna de foreign key
+			joinColumns = @JoinColumn(name = "product_id"),
+			// Define foreign key da outra entidade, no caso(category)
+			inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product() {
 	}
 
-	//Coleção SET não vai no construtor pois já está sendo instânciada.
+	// Coleção SET não vai no construtor pois já está sendo instânciada.
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
